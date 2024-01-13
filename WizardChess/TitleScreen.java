66f5329dccfbf9857f11851929a3f5c3428db25d
SoulEmp <Awesomez0legendz@gmail.com>
2024-01-13 11:51:52 -0500

    buttons, titlescreen, endscreen 1.1

---
+++ WizardChess/TitleScreen.java
@@ -0,0 +1,93 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * <html>
+ * <body>
+ * <p><strong>TitleScreen</strong> class extends <em>World</em> and represents the title screen of a game in Greenfoot.</p>
+ * <p>This class manages the display of the title screen, including background images and buttons, and handles user interactions to start the game.</p>
+ *
+ * <h3>Class Fields:</h3>
+ * <ul>
+ *     <li><strong>bg</strong> - The background image for the title screen.</li>
+ *     <li><strong>startButton</strong> - A button that starts the game when clicked.</li>
+ *     <li><strong>music</strong> - The background music for the title screen.</li>
+ *     <li><strong>title</strong> - The title display button.</li>
+ * </ul>
+ *
+ * @author David Guo, Dorsa Rohani
+ * @version 1.1 01/13/2023
+ */
+public class TitleScreen extends World
+{
+    // Background image
+    private GreenfootImage bg;
+    // Buttons which highlight when hovered over
+    private TextButton startButton;
+    // MP3 file for the title screen music
+    private static GreenfootSound music;
+    
+    private TextButton title;
+    /**
+     * <h3>Constructor:</h3>
+     * <p>Initializes the title screen with a specific background, sets up buttons, and configures background music.</p>
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1200, 740, 1);
+        // Set bg to background image
+        //bg = new GreenfootImage ("");
+        //setBackground(bg);
+        // Create new buttons for the variables
+        startButton = new TextButton("START", 90, 255, 255, 255, 255, 20, 147);
+
+        //title
+        //title = new TextButton("Title", 150, 255, 255, 255, 255, 20, 147);
+        
+        // Add buttons to the world
+        addObject(startButton, 600, 600);
+        //addObject(title, 0, 0);
+        // Assign the variable to the sound file name in folder & adjust volume
+        music = new GreenfootSound("stmatthewpassion.mp3");
+        music.setVolume(50);
+        // Add the title of the game
+        //addObject(new TitlePic("TitlePic.png"), getWidth()/2, getHeight()/4);
+    }
+
+    /**
+     * <p><strong>void act()</strong> - Checks for user interactions, particularly if the start button is clicked to begin the game.</p>
+     */
+    public void act(){
+        // checks if the player has clicked play and puts them into the game if they did
+        if(Greenfoot.mouseClicked(startButton)){
+            //Greenfoot.setWorld(new SettingsWorld());
+        }
+    }
+    
+    /**
+     * <p><strong>void started()</strong> - Plays the background music in a loop when the game starts.</p>
+     */
+    // Play song when the game starts
+    public void started() {
+        music.playLoop();
+    }
+    
+    /**
+     * <p><strong>void stopped()</strong> - Pauses the background music when the game is stopped.</p>
+     */
+    // Pause song if they stop the program
+    public void stopped() {
+        music.pause();
+    }
+    
+    /**
+     * <p><strong>static GreenfootSound getMusic()</strong> - Provides access to the background music.</p>
+     * <ul>
+     *     <li><strong>Return:</strong> GreenfootSound - The background music for the title screen.</li>
+     * </ul>
+     */
+    // Getter method for the music
+    public static GreenfootSound getMusic(){
+        return music;
+    }
+}
---
+++ WizardChess/Widget.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Superclass meant to organize different widget subclasses and used for debugging/testing.
+ * If there is a method which can be used in a couple subclasses, it will go in here.
+ * 
+ * @author David Guo
+ * @version 1.0 01/13/2024
+ */
+public class Widget extends Actor
+{
+    /**
+     * Act - do whatever the Widget wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
