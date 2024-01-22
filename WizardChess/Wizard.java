9e8a9f854ea42a96a2421b93d52a1fe9d6b4abcf
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2024-01-22 14:53:29 -0500

    creds

--- WizardChess/Wizard.java
+++ WizardChess/Wizard.java
@@ -4,7 +4,7 @@ import greenfoot.*;
  * Main class representing the player's wizard character.
  * Handles movement, actions, and attributes of the wizard.
  *
- * @author Jimmy Zhu, Mekaeel Malik
+ * @author Jimmy Zhu, Mekaeel Malik, Dorsa Rohani
  * @version January 21st, 2023
  */
 public class Wizard extends SuperSmoothMover {
