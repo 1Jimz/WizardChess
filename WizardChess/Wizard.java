967fca3cdfdfd36fa9a2aa99a65897cbbc7bfe54
1Jimz <JimmyJunjiZhu@gmail.com>
2024-01-24 16:28:37 -0500

    Revert "stuff"
    This reverts commit 8e05d535ca47cf56d411e9b9d6105533d9131631.

--- WizardChess/Wizard.java
+++ WizardChess/Wizard.java
@@ -151,6 +151,7 @@ public class Wizard extends SuperSmoothMover {
                 decreaseE(2);
             }
         }
+
         // Handle wizard taking damage and healing
         if (damaged) {
             updateHP(-50); // 50 is temp
@@ -160,7 +161,7 @@ public class Wizard extends SuperSmoothMover {
                 Greenfoot.setWorld(new EndScreen(true)); // wizard died rip
             }
         }
-        if(heal) {
+        if (heal) {
             updateHP(10);
             heal = false;
         }
