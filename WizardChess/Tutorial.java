9be4ee4fcd3089b2b49afa76186fdcf900e30636
SoulEmp <Awesomez0legendz@gmail.com>
2024-01-23 22:33:39 -0500

    pretty ok tutorial

--- WizardChess/Tutorial.java
+++ WizardChess/Tutorial.java
@@ -23,7 +23,7 @@ public class Tutorial extends Widget
         // Width, height, maxPageNum in order to make it modular
         width = 1200*5/6;
         height = 740*5/6;
-        maxPageNum = 5;
+        maxPageNum = 7;
         // Initialize variables to change button location
         xDist = 80; // from the edges left/right
         yDist = 20; // from the bottom
